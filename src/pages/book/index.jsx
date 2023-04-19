bb62ba5a66ab9f2f6cb10c179dfd087c8a55c8ab
The Hieu <thehieu236@gmail.com>
2023-04-19 15:42:54 +0700

    Chia layout và project structure

---
+++ src/pages/book/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookPage = () => {
+  return <div>Book page</div>;
+};
+
+export default BookPage;
---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactPage = () => {
+  return <div>content page</div>;
+};
+
+export default ContactPage;
